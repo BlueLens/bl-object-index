559a27f69f0b028724e7d44546e5ec99f5f5b153
Youngbok Yoon <master@bluehack.net>
2017-11-20 15:44:40 +0900

    Changed port to 50053

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo nvidia-docker run -dit --restart unless-stopped -p 50052:50052 bluelens/bl-detect:latest
+sudo nvidia-docker run -dit --restart unless-stopped -p 50053:50053 bluelens/bl-index:latest
